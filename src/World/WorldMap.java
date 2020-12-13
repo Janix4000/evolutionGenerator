ac7a93539679527b3d43d41e4c5d87d7c1fa7ac8
Jan_Izydorczyk <janix4000@gmail.com>
2020-12-13 12:49:44 +0100

    Added WorldMap class, not fully implemented

---
+++ src/World/WorldMap.java
@@ -0,0 +1,23 @@
+package World;
+
+import Utility.Vector2d;
+
+public class WorldMap implements IWorldBoundaries {
+    private final AnimalMap animals = new AnimalMap();
+    private final Vector2d size;
+    WorldMap(Vector2d size) {
+        this.size = size;
+    }
+
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+    }
+
+
+    @Override
+    public Vector2d getProperNextPosition(Vector2d nextPosition) {
+        int x = (nextPosition.x % size.x + size.x) % size.x;
+        int y = (nextPosition.y % size.y + size.y) % size.y;
+        return new Vector2d(x, y);
+    }
+}
