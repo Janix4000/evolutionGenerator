e534da338fb0060c58f996bd7a568b62048daebc
Jan_Izydorczyk <janix4000@gmail.com>
2020-12-18 21:30:57 +0100

    Added Rectangle

---
+++ src/Utility/Rectangle.java
@@ -0,0 +1,11 @@
+package Utility;
+
+public class Rectangle {
+    public Vector2d position;
+    public Vector2d size;
+
+    public Rectangle (Vector2d position, Vector2d size) {
+        this.position = position;
+        this.size = size;
+    }
+}
