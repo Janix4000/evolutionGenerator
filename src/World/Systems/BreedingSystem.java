c54ece7e6854ddbb0e37c03b290e521d1f83a3f9
Jan_Izydorczyk <janix4000@gmail.com>
2020-12-18 23:28:30 +0100

    Added BreedingSystem class

---
+++ src/World/Systems/BreedingSystem.java
@@ -0,0 +1,64 @@
+package World.Systems;
+
+import Utility.Vector2d;
+import World.Entities.Animal;
+import World.Map.IWorldMap;
+import World.Map.WorldMapCell;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+public class BreedingSystem {
+    private final IWorldMap map;
+
+    public BreedingSystem(IWorldMap map) {
+        this.map = map;
+    }
+
+    public ArrayList<Animal> getChildren(Iterator<WorldMapCell> it) {
+        ArrayList<Animal> children = new ArrayList<>();
+
+        while(it.hasNext()) {
+            var cell = it.next();
+            if(cell.sizeOfAnimals() < 2) {
+                continue;
+            }
+            var candidatesToReproduce = cell.getTopAnimals(2);
+            var par0 = candidatesToReproduce.get(0);
+            var par1 = candidatesToReproduce.get(1);
+            if(!par1.canBreed()) {
+                continue;
+            }
+            Animal child = new Animal(par0, par1);
+            Vector2d pos = generatePositionForChild(par0.getWorldPosition());
+            child.setPosition(pos);
+            children.add(child);
+        }
+
+        return children;
+    }
+
+    private Vector2d generatePositionForChild(Vector2d position) {
+        var posPositions = new ArrayList<Vector2d>(8);
+        for(int y = -1; y <= 1; ++y) {
+            for(int x = -1; x <= 1; ++x) {
+                if(x == 0 && y == 0) {
+                    continue;
+                }
+                var diff = new Vector2d(x, y);
+                posPositions.add(map.getProperNextPosition(position.add(diff)));
+            }
+        }
+        Collections.shuffle(posPositions);
+        for(var pos : posPositions) {
+            if(map.isOccupied(pos)) {
+                continue;
+            }
+            return  pos;
+        }
+        return posPositions.get(0);
+    }
+
+
+}
--- src/World/World.java
+++ src/World/World.java
@@ -4,6 +4,7 @@ import Utility.CoordinateTransformer;
 import Utility.Vector2d;
 import World.Entities.Animal;
 import World.Map.WorldMap;
+import World.Systems.BreedingSystem;
 import processing.core.PApplet;
 import processing.core.PGraphics;
 
@@ -42,49 +43,12 @@ public class World {
     }
 
     private void breedAnimals() {
-        ArrayList<Animal> children = new ArrayList<>();
-        for(var cell : worldMap) {
-            if(cell.sizeOfAnimals() < 2) {
-                continue;
-            }
-            var candidatesToReproduce = cell.getTopAnimals(2);
-            var par0 = candidatesToReproduce.get(0);
-            var par1 = candidatesToReproduce.get(1);
-            if(!par1.canBreed()) {
-                continue;
-            }
-            Animal child = new Animal(par0, par1);
-            Vector2d pos = generatePositionForChild(par0.getWorldPosition());
-            child.setPosition(pos);
-            children.add(child);
-        }
-
+        BreedingSystem breedingSystem = new BreedingSystem(worldMap);
+        var children = breedingSystem.getChildren(worldMap.iterator());
         for (var child : children) {
             addAnimal(child);
         }
     }
-
-    private Vector2d generatePositionForChild(Vector2d position) {
-        var posPositions = new ArrayList<Vector2d>(8);
-        for(int y = -1; y <= 1; ++y) {
-            for(int x = -1; x <= 1; ++x) {
-                if(x == 0 && y == 0) {
-                    continue;
-                }
-                var diff = new Vector2d(x, y);
-                posPositions.add(worldMap.getProperNextPosition(position.add(diff)));
-            }
-        }
-        Collections.shuffle(posPositions);
-        for(var pos : posPositions) {
-            if(worldMap.isOccupied(pos)) {
-                continue;
-            }
-            return  pos;
-        }
-        return posPositions.get(0);
-    }
-
 
     private void feedAnimalsWithGrass() {
         for(var cell : worldMap) {
